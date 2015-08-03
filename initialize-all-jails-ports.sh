a7b1eacc6fa97fae96adf6b8c6a2b293d4ddf10e
John Aylward <john@johnanddalene.net>
2015-08-03 16:06:38 -0400

    added message output to initialize-all script

--- initialize-all-jails-ports.sh
+++ initialize-all-jails-ports.sh
@@ -4,5 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
     jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
+    echo "-------- initializing ${jail} --------"
     $DIR/initialize-jail-ports.sh "${jail}"
+    echo "------- completed init ${jail} -------"
 done
