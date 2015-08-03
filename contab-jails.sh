10ad825bd1e4c58aa71384adde30dc79bd2e9d20
John J. Aylward <john.aylward@spacenet.com>
2015-08-03 12:54:06 -0400

    updated for use in FreeNAS using bash

--- contab-jails.sh
+++ contab-jails.sh
@@ -1,20 +1,30 @@
-#!/usr/local/bin/zsh
-JAIL_PORTS=/usr/jails/ports
-SHELL=/usr/local/bin/zsh                                                                                                                                                  
+#!/usr/local/bin/bash
+# modified for freenas from https://gist.github.com/takeshixx/7487381
+SHELL=/usr/local/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
+JAIL_PORTS=/mnt/jails/ports
 SECTION=' * * * * * * '
 DELIMITER='------------------------------'
 
-portsnap -p $JAIL_PORTS fetch extract &>/dev/null || echo "Updating ports tree failed!"
+#updates shared ports dir that is null mounted into each jail
+#portsnap -p $JAIL_PORTS fetch extract &>/dev/null || echo "Updating ports tree failed!"
+
+# if you don't have a shared ports mount, then use this.
+# this will take a long time to run for each jail.
+for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
+    jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
+    jexec $JID portsnap -p $JAIL_PORTS fetch extract &>/dev/null || echo "Updating ports tree failed for jail ${jail}!"
+done
 
 echo "# VULNERABILITIES"
 echo
 
-for jail in $(jails);do 
-        JID=$(jid $jail)
-        echo "--- ${jail} ---"
-        jexec $JID pkg audit 
-        echo $DELIMITER
+for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
+    jexec $JID portsnap -p $JAIL_PORTS fetch extract &>/dev/null
+    jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
+    echo "--- ${jail} ---"
+    jexec $JID pkg audit
+    echo $DELIMITER
 done
 
 echo
@@ -24,10 +34,10 @@ echo
 echo "# AVAILABLE UPDATES"
 echo
 
-for jail in $(jails);do 
-        JID=$(jid $jail)
-        echo "--- ${jail} ---"
-        jexec $JID portmaster -L --index-only| egrep '(ew|ort) version|total install'
-        echo $DELIMITER
-        echo
+for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
+    jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
+    echo "--- ${jail} ---"
+    jexec $JID portmaster -L --index-only| egrep '(ew|ort) version|total install'
+    echo $DELIMITER
+    echo
 done
