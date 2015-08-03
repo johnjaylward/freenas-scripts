94bc7b3ecf146ca0760c36304602a656d38b603d
John J. Aylward <john.aylward@spacenet.com>
2015-08-03 16:03:48 -0400

    changes to use portmaster for the initial package update

--- initialize-jail-ports.sh
+++ initialize-jail-ports.sh
@@ -5,7 +5,7 @@ echo "Updating PORTS with fetch/extract"
 jexec "${1}" portsnap -p $JAIL_PORTS fetch extract &>/dev/null || (echo "Updating ports tree failed for jail ${jail}!" && exit 1)
 
 jexec "${1}" pkg update
-jexec "${1}" pkg upgrade
 jexec "${1}" pkg install bash portmaster
+jexec "${1}" portmaster -a
 
 [ ! -s /etc/make.conf ] && echo 'WITH_PKGNG=yes' >> /etc/make.conf && pkg2ng
