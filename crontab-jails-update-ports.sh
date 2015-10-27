29509ce7b2cd5b5f0497be09c118639025a41bc0
John Aylward <john@johnanddalene.net>
2015-10-27 10:07:18 -0400

    latest script changes

--- crontab-jails-update-ports.sh
+++ crontab-jails-update-ports.sh
@@ -10,5 +10,5 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
 # this will take a long time to run for each jail.
 for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
     jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
-    jexec $JID portsnap -p $JAIL_PORTS fetch extract &>/dev/null || echo "Updating ports tree failed for jail ${jail}!"
+    jexec $JID portsnap fetch extract update &>/dev/null || echo "Updating ports tree failed for jail ${jail}!"
 done
--- update-all-jail-ports.sh
+++ update-all-jail-ports.sh
@@ -5,8 +5,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
     jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
     echo "-------- updating ${jail} --------"
-    jexec $JID portmaster -a
-    jexec $JID portmaster -t -y --clean-distfiles
+    #jexec $JID portmaster -P -a -y
+    jexec $JID portmaster -a -y
+    jexec $JID portmaster -s -y
+    #jexec $JID portmaster -t -y --clean-distfiles
     echo "-------- completed ${jail} -------"
     echo ""
     echo ""
