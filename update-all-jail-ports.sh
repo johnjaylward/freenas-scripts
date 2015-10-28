3410a243cd0a31532505906abc1af87ca5e36c48
John Aylward <john@johnanddalene.net>
2015-10-27 21:03:40 -0400

    updates

--- update-all-jail-ports.sh
+++ update-all-jail-ports.sh
@@ -1,14 +1,14 @@
 #!/usr/local/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 for JID in $(jls -h jid | grep '^[0-9]' | awk '{ print $1 }');do
     jail=$(jls -h jid name | grep "^$JID " | awk '{ print $2 }')
     echo "-------- updating ${jail} --------"
-    #jexec $JID portmaster -P -a -y
-    jexec $JID portmaster -a -y
-    jexec $JID portmaster -s -y
-    #jexec $JID portmaster -t -y --clean-distfiles
+    #jexec $JID portmaster -y -P -a
+
+    jexec $JID portmaster --no-confirm -adw
+    jexec $JID portmaster -y -y -d -s
+    
+    #jexec $JID portmaster -y -t --clean-distfiles
     echo "-------- completed ${jail} -------"
     echo ""
     echo ""
